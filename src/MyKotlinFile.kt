4864f3b9067cfdbf27b5ff0c4aef90d02de39415
TTawtet <gby211@yandex.ru>
2022-02-20 22:53:39 +0300

    add fun arrays

--- src/MyKotlinFile.kt
+++ src/MyKotlinFile.kt
@@ -1,4 +1,10 @@
+import java.util.Arrays
+
 fun main() {
+    arrayTest()
+}
+
+fun helloWorld(){
     print("Hello WORLD!!!!")
 }
 
@@ -21,5 +27,19 @@ fun timeOfDay() {
 fun templates(){
     val s = "abc"
     val str = "$s.length is ${s.length}" // evaluates to "abc.length is 3"
-    print(str)
+    println(str)
+}
+
+fun arrayTest(){
+    val array:IntArray = intArrayOf(10,23,324,324,0x12,0b10010101)
+    println(array.contentToString())
+
+    var arrayConstr = IntArray(10, {0})
+    println(arrayConstr.contentToString())
+
+    val asc = IntArray(5) { i -> (i+1) }
+    println(asc.contentToString())
+    val asc2 = IntArray(5) { i -> (i*i) }
+    println(asc2.contentToString())
+
 }

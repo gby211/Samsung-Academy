426b8d144b5c2b5f59e1bf62de2c10995f76f87c
TTawtet <gby211@yandex.ru>
2022-02-20 23:11:05 +0300

    add fun sumEven, and test ArrayList

--- src/MyKotlinFile.kt
+++ src/MyKotlinFile.kt
@@ -1,10 +1,11 @@
-import java.util.Arrays
+import java.util.*
+import kotlin.collections.ArrayList
 
 fun main() {
-    arrayTest()
+    sumEven()
 }
 
-fun helloWorld(){
+fun helloWorld() {
     print("Hello WORLD!!!!")
 }
 
@@ -24,22 +25,38 @@ fun timeOfDay() {
     println("Now - $timeOfDay")
 }
 
-fun templates(){
+fun templates() {
     val s = "abc"
     val str = "$s.length is ${s.length}" // evaluates to "abc.length is 3"
     println(str)
 }
 
-fun arrayTest(){
-    val array:IntArray = intArrayOf(10,23,324,324,0x12,0b10010101)
+fun arrayTest() {
+    val array: IntArray = intArrayOf(10, 23, 324, 324, 0x12, 0b10010101)
     println(array.contentToString())
 
-    var arrayConstr = IntArray(10, {0})
+    var arrayConstr = IntArray(10, { 0 })
     println(arrayConstr.contentToString())
 
-    val asc = IntArray(5) { i -> (i+1) }
+    val asc = IntArray(5) { i -> (i + 1) }
     println(asc.contentToString())
-    val asc2 = IntArray(5) { i -> (i*i) }
+    val asc2 = IntArray(5) { i -> (i * i) }
     println(asc2.contentToString())
 
+}
+
+fun sumEven() {
+    var rand = Random()
+    val intAr = IntArray(10) { rand.nextInt(300) }
+    var sum = 0
+    val intArEv: ArrayList<Int> = ArrayList()
+    for (value in intAr){
+        if(value%2==0) {
+            sum += value
+            intArEv.add(value)
+        }
+    }
+    println("Initial array - ${intAr.contentToString()}")
+    println("ArrayList of even numbers - $intArEv")
+    println("Sum of even numbers - $sum")
 }
